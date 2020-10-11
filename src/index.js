25710d0b312dbc1f814ce88a9e533990380c0ccb
TFX-98 <66826440+TFX-98@users.noreply.github.com>
2020-10-12 01:26:39 +0300

    Add Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let d = n.toString().split('');
+    d[0] == '-' ? d.splice(0,1) : '';
+    d.reverse();
+    n = d.join('');
+    return +n;
 }
